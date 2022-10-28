ab4b6f6450782a9f17a51bba883e9485030f8b5b
white <white@example.com>
2022-10-28 17:16:47 +0500

    add method

--- OPD_prject/OPD_prject/Program.cs
+++ OPD_prject/OPD_prject/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("hello");
-//Console.ReadKey();
+﻿
+public class HelloWorld1
+{
+    public static void HelloWorld()
+    {
+        Console.WriteLine("hello");
+    }
+
+
+    public static void Main()
+    {
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("hello");
+        //Console.ReadKey();
+    }
+}
